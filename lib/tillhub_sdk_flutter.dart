72f7bb4273f7f347efd264517248337e8e8525ad
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2019-05-22 18:13:55 +0200

    feat: get base deps

--- lib/tillhub_sdk_flutter.dart
+++ lib/tillhub_sdk_flutter.dart
@@ -1,9 +1,23 @@
 library tillhub_sdk_flutter;
 
+import 'package:dio/dio.dart';
+
 /// Tillhub SDK.
 class Tillhub {
+  Dio _client;
+  TillhubSDKOptions options;
+
+  Tillhub([TillhubSDKOptions options]) {
+    if (options == null) {
+      options = new TillhubSDKOptions();
+    }
+    this.options = options;
+  }
+
   /// Initialises an API instance
   Tillhub init() {
     return new Tillhub();
   }
 }
+
+class TillhubSDKOptions {}
